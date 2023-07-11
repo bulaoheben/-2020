ae238395c30f486c52c1d05bbc4675bc401d6de1
wj20021016 <104957136+wj20021016@users.noreply.github.com>
2023-07-11 09:52:51 +0800

    新增 事件截图获取 的接口

--- app/mod_event/models.py
+++ app/mod_event/models.py
@@ -10,6 +10,7 @@ class EventInfo(db.Model):
     event_location = db.Column(db.String(200))
     event_desc = db.Column(db.String(200))
     oldperson_id = db.Column(db.Integer)
+    pic_url = db.Column(db.String(100))
 
     def to_dict(self):
         return {
@@ -18,5 +19,6 @@ class EventInfo(db.Model):
             'event_date': self.event_date.strftime('%Y-%m-%d %H:%M:%S') if self.event_date else None,
             'event_location': self.event_location,
             'event_desc': self.event_desc,
-            'oldperson_id': self.oldperson_id
+            'oldperson_id': self.oldperson_id,
+            'pic_url': self.pic_url
         }
--- app/mod_video/video.py
+++ app/mod_video/video.py
@@ -2,14 +2,13 @@ import os
 
 from flask import Response, Blueprint, request, send_file
 import cv2
-from the_old_fall.api.stranger_facial_expression import strangerFacialExpression
 
 app_video = Blueprint('video', __name__)
 camera = cv2.VideoCapture(0)  # 0表示默认的摄像头
+image_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) + "/static/"
 
 
 def generate_frames():
-    buf = strangerFacialExpression()
     counter = 0
     while True:
         # 读取摄像头视频帧
@@ -17,16 +16,15 @@ def generate_frames():
         if not success:
             break
         else:
+            flag = False
             counter += 1
-            # 获得事件id
-            (event_id, processed_frame, flag) = buf.strangerFacialExpressionAPI(success, frame, counter)
+            # 获得事件id，处理后的图像processed_frame，是否出现故障flag，事件对应的描述description
+            # (event_id, processed_frame, flag, description) = buf.strangerFacialExpressionAPI(success, frame, counter)
             if flag:
                 break
 
-            # 若检测到事件，则插入数据库
-
             # 将处理后的帧转换为图片格式
-            ret, buffer = cv2.imencode('.jpg', processed_frame)
+            ret, buffer = cv2.imencode('.jpg', frame)
             frame_bytes = buffer.tobytes()
 
             # 返回处理后的图片给前端
@@ -40,18 +38,21 @@ def generate_frames():
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# @app_video.route('/get_pic', methods=['POST'])
-# def get_image():
-#     # 从前端请求中获取图片路径
-#     image_path = request.json.get('image_path')
-#
-#     # 构建完整的图片路径
-#     full_image_path = os.path.join('path/to/images', image_path)
-#
-#     # 检查图片是否存在
-#     if os.path.exists(full_image_path):
-#         # 设置正确的MIME类型和Content-Disposition头部信息
-#         return send_file(full_image_path, mimetype='image/jpeg', as_attachment=False)
-#     else:
-#         # 图片不存在时返回错误信息
-#         return 'Image not found', 404
+
+# 传入事件图片名称，获取图片
+@app_video.route('/get_pic', methods=['POST'])
+def get_image():
+    # 从前端请求中获取图片路径
+    image_path = request.json.get('image_name')
+
+    # 构建完整的图片路径
+    full_image_path = os.path.join(image_root_path, image_path)
+
+    # 检查图片是否存在
+    if os.path.exists(full_image_path):
+        # 设置正确的MIME类型和Content-Disposition头部信息
+        return send_file(full_image_path, mimetype='image/jpg', as_attachment=False)
+    else:
+        # 图片不存在时返回错误信息
+        response = {"code": "23", "message": "Image not found."}
+        return response
